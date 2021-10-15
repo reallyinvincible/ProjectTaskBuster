847116345c2be58d6ef0b95079a9d9fc93069841
Sparsh <mailtosparshsri@gmail.com>
2021-10-16 03:45:25 +0530

    added insert note usecase

---
+++ app/src/main/java/app/sparsh/taskbuster/business/data/util/GenericErrors.kt
@@ -0,0 +1,8 @@
+package app.sparsh.taskbuster.business.data.util
+
+object GenericErrors {
+
+    const val ERROR_UNKNOWN = "Unknown error"
+    const val INVALID_STATE_EVENT = "Invalid state event"
+
+}
---
+++ app/src/main/java/app/sparsh/taskbuster/framework/presentation/notelist/NoteListFragment.kt
@@ -0,0 +1,6 @@
+package app.sparsh.taskbuster.framework.presentation.notelist
+
+import androidx.fragment.app.Fragment
+
+class NoteListFragment: Fragment() {
+}
